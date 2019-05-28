a8372ce1a5dc8e2968996c2a11a35920f0973f10
bickelmann brice <bickelmann.b@gmail.com>
2019-05-28 15:58:00 +0200

    boxe_controller_done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 resources :boxes
+  # delete "boxes/:id", to: "boxes#destroy", as: "destroy"
 resources :reservations, except: [:update, :edit]
 resources :users, except: :index
-  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

291c1f0736ee1ab43546e076e563412b989c13dc
Florian Couraud <f.couraud13@gmail.com>
2019-05-28 11:35:08 +0200

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_085953) do
+ActiveRecord::Schema.define(version: 2019_05_28_090742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boxes", force: :cascade do |t|
+    t.string "description"
+    t.string "caracteristics"
+    t.string "address"
+    t.string "name"
+    t.integer "price_per_day"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boxes_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "total_price"
+    t.bigint "boxe_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boxe_id"], name: "index_reservations_on_boxe_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +55,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_085953) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "boxes", "users"
+  add_foreign_key "reservations", "boxes", column: "boxe_id"
+  add_foreign_key "reservations", "users"
 end

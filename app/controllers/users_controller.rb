9dcd73d9d09a61d050e9aab6c0d419fecf87fe8a
Florian Couraud <f.couraud13@gmail.com>
2019-05-28 12:53:53 +0200

    models validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,32 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'users/new'
+    end
   end
 
   def show
-  end
-
-  def destroy
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
   end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,19 @@
 class Reservation < ApplicationRecord
   belongs_to :boxe
   belongs_to :user
+  validates :start_date?
+  validates :end_date_after_start_date?
+  validates :total_price, presence: true
+
+  def end_date_after_start_date?
+    if end_date < start_date
+      errors.add :end_date, "Must be after start date."
+    end
+  end
+
+  def start_date?
+    if start_date < Date.today
+      errors.add :start_date, "Cannot be from the past."
+    end
+  end
 end

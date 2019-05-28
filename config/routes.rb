36911ede0d219b776f317f3d8385306441b93274
Wilfried Bessovi <wbsvnet@gmail.com>
2019-05-28 15:54:42 +0200

    Ajout des seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+devise_for :users
 resources :boxes
 resources :reservations, except: [:update, :edit]
 resources :users, except: :index
-  devise_for :users
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+user1 = User.create(first_name: "Wilfried", last_name: "Bessovi", email: "wbsvnet@gmail.com", password: "123456")
+boxe1 = Boxe.create(name: "Box1", description: "Location box - Box / garage", caracteristics: "Surface: 20m2", address:"23 rue Paradis", price_per_day: 20, user: user1)
+
+user2 = User.create(first_name: "Jessica", last_name: "Fiedler", email: "jess.fiedler83860@gmail.com", password: "123456")
+boxe2 = Boxe.create(name: "Box2", description: "Location box - Box / garage", caracteristics: "Surface: 30m2", address:"4 rue Republique", price_per_day: 30, user: user2)
+
+user3 = User.create(first_name: "Florian", last_name: "Couraud", email: "f.couraud13@gmail.com", password: "123456")
+boxe3 = Boxe.create(name: "Box3", description: "Location box - Box / garage", caracteristics: "Surface: 40m2", address:"27 Av du Prado", price_per_day: 40, user: user3)
+
+user4 = User.create(first_name: "Brice", last_name: "Bickelmann", email: "bickelmann.b@gmail.com", password: "123456")
+boxe4 = Boxe.create(name: "Box4", description: "Location box - Box / garage", caracteristics: "Surface: 25m2", address:"4 rue Marechal", price_per_day: 25, user: user4)
+
+user5 = User.create(first_name: "John", last_name: "Doe", email: "jdoe@gmail.com", password: "123456")
+user6 = User.create(first_name: "Maria", last_name: "Carey", email: "mcarey@gmail.com", password: "123456")
+user7 = User.create(first_name: "Winston", last_name: "Churchill", email: "wchurchill@gmail.com", password: "123456")
+
+reservation1 = Reservation.create(start_date:Time.parse("2019-06-03"), end_date:Time.parse("2019-09-03"), user_id: user5, boxe_id: boxe3, total_price: boxe3.price_per_day)
+reservation2 = Reservation.create(start_date:Time.parse("2019-07-03"), end_date:Time.parse("2019-08-03"), user_id: user6, boxe_id: boxe4, total_price: boxe4.price_per_day)

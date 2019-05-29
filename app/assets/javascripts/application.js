731b1c7ba3890c834a2ac3bc57f37b9a611b4de5
Wilfried Bessovi <wbsvnet@gmail.com>
2019-05-29 22:27:53 +0200

    Ajout Booking form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,9 @@
 //= require rails-ujs
 //= require_tree .
+//= require bootstrap-datepicker
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
+
+flatpickr(".datepicker", {
+
+});

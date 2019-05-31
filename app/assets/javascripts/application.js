da66f844ddf6fb6c19c58e24debb6a920769915c
Jessica Fiedler <jess.fiedler83860@gmail.com>
2019-05-31 11:45:47 +0200

    Logout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,3 @@
 //= require rails-ujs
 //= require_tree .
 //= require bootstrap-datepicker
-import flatpickr from 'flatpickr'
-import 'flatpickr/dist/flatpickr.min.css'
-
-flatpickr(".datepicker", {
-
-});
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-devise_for :users
-resources :users, only: [:show]
-resources :boxes do
-  resources :reservations, only: [:create]
-end
-resources :reservations, only: [:index, :show, :destroy]
+  devise_for :users
+  resources :users, only: [:show]
+  resources :boxes do
+    resources :reservations, only: [:create]
+  end
+  resources :reservations, only: [:index, :show, :destroy]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
